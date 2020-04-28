409331cf88d2ae7d3f068b688ce66aef9cccb6b4
vikrapolu7 <vikramrapolu@gmail.com>
2020-04-29 01:08:34 +0530

    Hi

---
+++ workspace/learnajax/src/main/java/com/vikram/learnajax/LearnajaxApplication.java
@@ -0,0 +1,13 @@
+package com.vikram.learnajax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnajaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnajaxApplication.class, args);
+	}
+
+}
---
+++ workspace/learnajax/src/test/java/com/vikram/learnajax/LearnajaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikram.learnajax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnajaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
